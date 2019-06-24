83c013d2df734518eea5da7f5f7bf92bcc0470bb
Benjamin Durham <ben.dev033@gmail.com>
2019-06-24 11:00:08 -0400

    first commit

---
+++ RestaurantRateMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RestaurantRateMVC.Startup))]
+namespace RestaurantRateMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
