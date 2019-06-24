24e0fbf872e7edf99c2d97b3b0535944d5ec4d1e
Benjamin Durham <ben.dev033@gmail.com>
2019-06-24 17:00:12 -0400

    details functionality

--- RestaurantRateMVC/Controllers/RestaurantController.cs
+++ RestaurantRateMVC/Controllers/RestaurantController.cs
@@ -70,6 +70,23 @@ namespace RestaurantRateMVC.Controllers
         }
 
 
+        // GET: Restaurant/Details/{id}
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if(restaurant == null)
+            {
+                return HttpNotFound();
+            }
+                return View(restaurant);
+            
+        }
+
+
         // GET: Restaurant/Delete/{id}
         public ActionResult Delete(int? id)
         {
